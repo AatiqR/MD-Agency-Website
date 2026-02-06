df06053598a049804e7705d297c2b514313c2144
AatiqR <aatiq9527@gmail.com>
2026-02-06 12:29:43 +0500

    ff

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
+// import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
